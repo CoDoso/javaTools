949c4db76dbffc660510a91d3f614ee98e048772
CoDoso <155891549+CoDoso@users.noreply.github.com>
2024-10-21 00:38:57 +0200

    Small Feature Extension
    Added Background Extension and Dynamic Frame SIze

--- javaTools/draw/BarChart.java
+++ javaTools/draw/BarChart.java
@@ -10,48 +10,71 @@ import javaTools.generate.RGBColor;
 public class BarChart extends JFrame {
     
 	private int[] data;
+	private String background = null;
 	/** <b><u>+ Draw a Bar-Chart of your Data! +</u></b><br>
 	 *@param data ( int[] ) The Data that you want to be visualized !
 	 */
 	public BarChart(int[] data) 
 	{
-        this.data = data;
+	this.data = data;
+        if (data.length <= 8) 
+        {
         setSize(500, 500);
-        setLocation(1000, 300);
+        setLocation(1000, 300); 
+        } else {
+        	setSize(500+(40*data.length), 500);
+            setLocation(1000-(20*data.length), 300);
+        }
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("BarChart");
         setVisible(true);
         getContentPane().setBackground(new Color(255,255,255));
     }
-    public void paint(Graphics stift)
+public void addBackground(String Design) 
     {
-    	super.paint(stift);
-    	stift.setColor(Color.BLACK);
-    	stift.fillRect(25, 450, ((data.length)*50)+45, 27);
-    	stift.drawRect(25, 75, ((data.length)*50)+45, 15*25+27);
-    	stift.fillRect(18, 75, 27, 403);
-    	
+    	for (String design :Window.design_list)
+    		if (design.equals(Design))
+        	{
+        		background = Design;
+            	repaint();
+            	break;
+        	}
+    }
+    public void paint(Graphics barChartFrame)
+    {
+    	super.paint(barChartFrame);
+    	if (background != null)
+        	Window.fillBackground(barChartFrame, background, getSize().width, 500);
+    	{   // Value Display Bars
+    		barChartFrame.setColor(Color.BLACK);
+        	barChartFrame.fillRect(25, 450, ((data.length)*50)+45, 27);
+        	barChartFrame.fillRect(18, 75, 27, 403);
+        	barChartFrame.setColor(Color.WHITE);
+        	int[] x = {18, 45, 45, (data.length)*50+70, (data.length)*50+70, 18};
+        	int[] y = {75, 75, 450, 450, 477, 477};
+        	barChartFrame.drawPolygon(x, y, 6);
+    	}
+    	// Side Bar Display
     	int num = 14;
     	for (int y = 100; y != 475; y+=25)
     	{
-    		stift.setColor(Color.BLACK);
-    		stift.drawLine(50, y, 35, y);
-    		stift.setColor(Color.WHITE);
-    		stift.drawString(num+"", 25, y+5);
+    		barChartFrame.setColor(Color.BLACK);
+    		barChartFrame.drawLine(50, y, 35, y);
+    		barChartFrame.setColor(Color.WHITE);
+    		barChartFrame.drawString(num+"", 25, y+5);
     		num -= 1;
     	}
+    	// Pillars
     	int i = 0;
     	for (int x = 70; i != data.length;x += 50)
     	{
-    		stift.setColor(RGBColor.get_random());
-    		stift.fillRect(x, (450 - (data[i] * 25)), 30, (data[i] * 25)+1);
-        	stift.setColor(Color.WHITE);
-    		stift.drawString(data[i]+"", x +12, 470);
+    		barChartFrame.setColor(RGBColor.get_random());
+    		barChartFrame.fillRect(x, (450 - (data[i] * 25)), 30, (data[i] * 25)+1);
+    		barChartFrame.setColor(Color.BLACK);
+    		barChartFrame.drawRect(x, (450 - (data[i] * 25)), 30, (data[i] * 25)+1);
+    		barChartFrame.setColor(Color.WHITE);
+    		barChartFrame.drawString(data[i]+"", x +12, 470);
     		i++;
     	}
-    	stift.setColor(Color.WHITE);
-    	int x[] = {18, 50, 50, (data.length)*50+70, (data.length)*50+70, 18};
-    	int y[] = {75, 75, 450, 450, 477, 477};
-    	stift.drawPolygon(x, y, 6);
     }
 }
